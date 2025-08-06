37480d57f33fb15bbfd42141bca1ea6063997782
User <user@MacBook-Pro-de-User.local>
2025-08-06 13:38:34 -0300

    chore: add drizzle setup with initial schema

---
+++ src/db/schema.ts
@@ -0,0 +1,59 @@
+import { relations } from "drizzle-orm";
+import { integer, pgEnum, pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core";
+
+export const userTable = pgTable("users", {
+	id: uuid().primaryKey().defaultRandom(),
+	name: text().notNull(),
+});
+
+export const categoryTable = pgTable("categories", {
+	id: uuid().primaryKey().defaultRandom(),
+	name: text().notNull(),
+	slug: text().notNull().unique(),
+	createdAt: timestamp("created_at").notNull().defaultNow(),
+});
+
+export const categoryRelations = relations(categoryTable, ({ many }) => {
+	return {
+		products: many(productTable),
+	}
+});
+
+export const productTable = pgTable("products", {
+	id: uuid().primaryKey().defaultRandom(),
+	categoryId: uuid("category_id").notNull().references(() => categoryTable.id),
+	name: text().notNull(),
+	slug: text().notNull().unique(),
+	description: text().notNull(),
+	createdAt: timestamp("created_at").notNull().defaultNow(),
+});
+
+export const productVariantTable = pgTable("product_variants", {
+	id: uuid().primaryKey().defaultRandom(),
+	productId: uuid("product_id").notNull().references(() => productTable.id),
+	name: text().notNull(),
+	slug: text().notNull().unique(),
+	priceInCents: integer("price_in_cents").notNull(),
+	color: text().notNull(),
+	imageUrl: text("image_url").notNull(),
+	createdAt: timestamp("created_at").notNull().defaultNow(),
+});
+
+export const productRelations = relations(productTable, ({ one, many }) => {
+	return {
+		category: one(categoryTable, {
+			fields: [productTable.categoryId],
+			references: [categoryTable.id],
+		}),
+		variants: many(productVariantTable),
+	}
+});
+
+export const productVariantsRelations = relations(productVariantTable, ({ one }) => {
+	return {
+		category: one(productTable, {
+			fields: [productVariantTable.productId],
+			references: [productTable.id],
+		}),
+	}
+});
